7012b726feb9ebe006e5f8e800204a120e93ebbb
Sarabe89 <ezealeguzman@gmail.com>
2023-11-05 19:04:19 -0300

    colecc static

--- cumpleaños/settings.py
+++ cumpleaños/settings.py
@@ -123,7 +123,8 @@ locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
